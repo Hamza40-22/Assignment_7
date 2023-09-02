29d600a6a5e798219c2422a038ef289881eb3538
Hamza40-22 <95353930+Hamza40-22@users.noreply.github.com>
2023-09-02 05:05:45 +0500

    Add files via upload

---
+++ JS_chapter1.js
@@ -0,0 +1,26 @@
+// Q#1
+
+ 
+//alert("Hey!! this is my first Javascript textbox ever made 🤩")
+
+
+// Q#2
+// alert("Erorr! Please enter a valid password")
+
+// Q#3
+
+
+// alert("Welcome to JS Land.... \n Happy Happy Coding!!!😎 ")
+
+
+
+
+// Q#4
+
+// alert("Welcome to JS Land... ")
+// alert("Happy Coding!")
+
+
+// Q#5
+// alert("Hey!! I can run JS through my web browser window")
+
---
+++ JS_chapter2.js
@@ -0,0 +1,28 @@
+// Q#1
+var usrename;
+// Q#2
+var my_Name = "Syed Hamza Ali";
+// Q#3
+var message = "Hello World!";
+alert(message)
+// Q#4
+alert(my_Name)
+var age ="I'm 21 yo \n";
+alert(age);
+var edu ="certified Mobile Application Development";
+alert (edu);
+// Q#5
+var pizza = "Pizza\nPizz\nPiz\nPi\nP"
+alert(pizza);
+// Q#6
+var email = "My Email address is syedhamza21965@gmail.com ";
+alert(email);
+// Q#7
+var learn = "I'm tryning to Learn from the  Book “A smarter way to learn JavaScript”.";
+alert(learn);
+// Q#8
+var content = "yeah! I can write HTML content through JavaScript ";
+document.write(content);
+// Q#9
+var end = "“▬▬▬▬▬▬▬▬▬ஜ ۩۞۩ ஜ▬▬▬▬▬▬▬▬▬”";
+alert(end);
